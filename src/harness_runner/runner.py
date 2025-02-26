08dcfa9427f005691bcd39ff2c19454a77520b72
Mike Turner <mike.turner@anu.edu.au>
2025-02-26 11:27:18 +0800

    Adds harness runner that can proxy requests and responses

---
+++ src/harness_runner/runner.py
@@ -0,0 +1,34 @@
+from aiohttp import client, web
+
+SERVER_URL = "http://localhost:8000"
+MOUNT_POINT = "/"
+
+
+async def handle_all_request_types(request):
+    proxy_path = request.match_info.get("proxyPath", "No proxyPath placeholder defined")
+    local_path = request.rel_url.path_qs
+    remote_url = SERVER_URL + local_path
+
+    print(f"{proxy_path=} {local_path=} {remote_url=}")
+
+    # Forward the request to the reference server
+    async with client.request(
+        request.method, remote_url, headers=request.headers.copy(), allow_redirects=False, data=await request.read()
+    ) as response:
+        headers = response.headers.copy()
+        body = await response.read()
+        return web.Response(headers=headers, status=response.status, body=body)
+
+
+def create_application():
+    app = web.Application()
+
+    # Add catch-all route for proxying all other requests to CSIP-AUS reference server
+    app.router.add_route("*", MOUNT_POINT + "{proxyPath:.*}", handle_all_request_types)
+
+    return app
+
+
+if __name__ == "__main__":
+    app = create_application()
+    web.run_app(app, port=8080)
