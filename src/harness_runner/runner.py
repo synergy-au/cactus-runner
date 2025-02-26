baca31fb1daf8c1f9a2fa9ec57f1becea7250548
Mike Turner <mike.turner@anu.edu.au>
2025-02-26 11:40:40 +0800

    Adds routes for test runner endpoints (start, status and finalize)

--- src/harness_runner/runner.py
+++ src/harness_runner/runner.py
@@ -1,3 +1,4 @@
+import http
 from pathlib import Path
 
 import yaml
@@ -5,6 +6,28 @@ from aiohttp import client, web
 
 SERVER_URL = "http://localhost:8000"
 MOUNT_POINT = "/"
+
+
+async def start_test_procedure(request: web.Request):
+    global current_test_procedure
+
+    return web.Response(status=http.HTTPStatus.CREATED, text="Test Procedure Started")
+
+
+async def finalize_test_procedure(request):
+    global current_test_procedure
+
+    current_test_procedure = None
+
+    return web.Response(status=http.HTTPStatus.OK, text="Test Procedure Finalized")
+
+
+async def test_procedure_status(request):
+    if current_test_procedure is not None:
+        text = "Test procedure running"
+    else:
+        text = "No test procedure running"
+    return web.Response(status=http.HTTPStatus.OK, text=text)
 
 
 async def handle_all_request_types(request):
@@ -26,6 +49,11 @@ async def handle_all_request_types(request):
 def create_application():
     app = web.Application()
 
+    # Add routes for Test Runner
+    app.router.add_route("POST", MOUNT_POINT + "start", start_test_procedure)
+    app.router.add_route("POST", MOUNT_POINT + "finalize", finalize_test_procedure)
+    app.router.add_route("GET", MOUNT_POINT + "status", test_procedure_status)
+
     # Add catch-all route for proxying all other requests to CSIP-AUS reference server
     app.router.add_route("*", MOUNT_POINT + "{proxyPath:.*}", handle_all_request_types)
 
@@ -39,6 +67,8 @@ def read_test_procedure_definitions(path: Path) -> dict:
 
 if __name__ == "__main__":
     test_procedures: dict = read_test_procedure_definitions(path=Path("config/test_procedure.yaml"))
+
+    current_test_procedure = None
 
     app = create_application()
     web.run_app(app, port=8080)
