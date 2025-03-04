2161510c0cd9717e8ad13036585666d6341cc467
Mike Turner <mike.turner@anu.edu.au>
2025-03-04 15:34:59 +0800

    Adds code and dependency (psycopg) for executing sql files

--- src/harness_runner/precondition.py
+++ src/harness_runner/precondition.py
@@ -1,11 +1,22 @@
 import logging
 from pathlib import Path
 
+from psycopg import Connection
+
 logger = logging.getLogger(__name__)
 
 
 class UnableToApplyDatabasePrecondition(Exception):
     pass
+
+
+def execute_sql_file_for_connection(connection: Connection, path_to_sql_file: Path) -> None:
+    with open(path_to_sql_file) as f:
+        sql = f.read()
+
+    with connection.cursor() as cursor:
+        cursor.execute(sql)
+        cursor.commit()
 
 
 def apply_db_precondition(precondition: str):
