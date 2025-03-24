0457ddd7f773d84cf1e1d6736b3007a508128740
Mike Turner <mike.turner@anu.edu.au>
2025-03-24 10:17:29 +0800

    Refactors client example to use models and hides use of aiohttp

--- examples/client.py
+++ examples/client.py
@@ -1,17 +1,34 @@
 import asyncio
 
-import aiohttp
-
-from cactus_runner.client import RunnerClient
+from cactus_runner.client import (
+    ClientSession,
+    ClientTimeout,
+    RunnerClient,
+    TestProcedureId,
+)
+from cactus_runner.models import (
+    ActiveTestProcedureStatus,
+    LastProxiedRequest,
+    RunnerCapabilities,
+)
 
 
 async def main():
-    timeout = aiohttp.ClientTimeout(total=30)
+    timeout = ClientTimeout(total=30)
     base_url = "http://localhost:8080/"
-    async with aiohttp.ClientSession(base_url=base_url, timeout=timeout) as session:
-        print(await RunnerClient.status(session=session))
-        print(await RunnerClient.capabilities(session=session))
-        print(await RunnerClient.last_request(session=session))
+    async with ClientSession(base_url=base_url, timeout=timeout) as session:
+        status: ActiveTestProcedureStatus = await RunnerClient.status(session=session)
+        print(status)
+
+        capabilities: RunnerCapabilities = await RunnerClient.capabilities(session=session)
+        print(capabilities)
+
+        last_request: LastProxiedRequest = await RunnerClient.last_request(session=session)
+        print(last_request)
+
+        test_id = TestProcedureId.ALL_01
+        # await RunnerClient.start(session=session, test_id=test_id, aggregator_certificate=None)
+        # await RunnerClient.finalize(session=session)
 
 
 if __name__ == "__main__":
