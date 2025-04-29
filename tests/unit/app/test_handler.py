076d0fafbe9dbe8f71dc4a77ba84dc0c8b74cb86
Mike Turner <mike.turner@anu.edu.au>
2025-04-29 14:32:38 +0800

    Adds tests for finalize handler

--- tests/unit/app/test_handler.py
+++ tests/unit/app/test_handler.py
@@ -1,3 +1,4 @@
+import http
 from unittest.mock import MagicMock
 
 import pytest
@@ -6,6 +7,48 @@ from aiohttp.web import Response
 from cactus_runner.app import handler
 from cactus_runner.app.shared import APPKEY_RUNNER_STATE
 from cactus_runner.models import ClientInteraction, RunnerStatus
+
+
+@pytest.mark.asyncio
+async def test_finalize_handler(mocker):
+    """
+    `mocker` is a fixture provided by the `pytest-mock` plugin
+    """
+
+    request = MagicMock()
+    get_active_runner_status_spy = mocker.spy(handler.status, "get_active_runner_status")
+    create_response_spy = mocker.spy(handler.finalize, "create_response")
+    mocker.patch("cactus_runner.app.finalize.get_zip_contents")
+
+    response = await handler.finalize_handler(request=request)
+
+    assert isinstance(response, Response)
+    create_response_spy.assert_called_once()
+    get_active_runner_status_spy.assert_called_once()
+
+
+@pytest.mark.asyncio
+async def test_finalize_handler_resets_runner_state(mocker):
+    request = MagicMock()
+    request.app[APPKEY_RUNNER_STATE].request_history = [None]  # a non-empty list stand-in
+    mocker.patch("cactus_runner.app.finalize.create_response")
+
+    _ = await handler.finalize_handler(request=request)
+
+    assert request.app[APPKEY_RUNNER_STATE].active_test_procedure is None
+    assert not request.app[APPKEY_RUNNER_STATE].request_history  # the list should be empty
+
+
+@pytest.mark.asyncio
+async def test_finalize_handler_handles_no_active_test_procedure():
+
+    request = MagicMock()
+    request.app[APPKEY_RUNNER_STATE].active_test_procedure = None
+
+    response = await handler.finalize_handler(request=request)
+
+    assert isinstance(response, Response)
+    assert response.status == http.HTTPStatus.BAD_REQUEST
 
 
 @pytest.mark.asyncio
