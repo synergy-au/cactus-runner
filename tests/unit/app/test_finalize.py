2fe8d2f3a058e08c52f7de025e1f64bf905c210f
Mike Turner <mike.turner@anu.edu.au>
2025-04-29 14:31:48 +0800

    Adds tests for finalize helper functions

---
+++ tests/unit/app/test_finalize.py
@@ -0,0 +1,59 @@
+import io
+import random
+import string
+import tempfile
+import zipfile
+
+import pytest
+from aiohttp.web import Response
+
+from cactus_runner.app import finalize
+
+
+def test_get_zip_contents(mocker):
+    """
+    NOTE: This test uses a mock to disable the database dump and so doesn't
+        verify the 'envoy_db.dump' is written into the zip archive.
+    """
+
+    def random_string(length: int) -> str:
+        return "".join(random.choices(string.ascii_uppercase + string.digits, k=length))
+
+    finalize.DATABASE_URL = "Placeholder"
+    subprocess_run_mock = mocker.patch.object(finalize.subprocess, "run")  # prevent db dump
+    json_status_summary = random_string(length=100)
+    contents_of_logfile = bytes(random_string(length=100), encoding="utf-8")
+
+    with tempfile.NamedTemporaryFile(delete_on_close=False) as logfile:
+        logfile.write(contents_of_logfile)
+        logfile.close()
+
+        zip_contents = finalize.get_zip_contents(json_status_summary=json_status_summary, runner_logfile=logfile.name)
+
+    zip = zipfile.ZipFile(io.BytesIO(zip_contents))
+
+    assert isinstance(zip_contents, bytes)
+    assert zip.read("test_procedure_summary.json") == bytes(json_status_summary, encoding="utf-8")
+    assert zip.read("cactus_runner.jsonl") == contents_of_logfile
+    subprocess_run_mock.assert_called_once()
+
+
+def test_get_zip_contents_raises_databasedumperror(mocker):
+    finalize.DATABASE_URL = None
+    mocker.patch.object(finalize.shutil, "copyfile")  # prevent logfile copying
+
+    with pytest.raises(finalize.DatabaseDumpError):
+        _ = finalize.get_zip_contents(json_status_summary="", runner_logfile="")
+
+
+def test_create_response(mocker):
+    mocked_zip_contents = random.randbytes(50)
+    get_zip_contents_mock = mocker.patch("cactus_runner.app.finalize.get_zip_contents")
+    get_zip_contents_mock.return_value = mocked_zip_contents
+
+    response = finalize.create_response(json_status_summary="", runner_logfile="")
+
+    assert isinstance(response, Response)
+    assert response.status == 200
+    assert response.content_type == "application/zip"
+    assert response.body == mocked_zip_contents
