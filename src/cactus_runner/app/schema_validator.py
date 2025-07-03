418044842a655797bed59464a3a27f92504314a5
Joe Crowley <joseph.crowley@synergy.net.au>
2025-07-03 16:02:11 +0800

    added schema definitions for v1.3-beta/storage and updated validator to suit

--- src/cactus_runner/app/schema_validator.py
+++ src/cactus_runner/app/schema_validator.py
@@ -4,24 +4,24 @@ from pathlib import Path
 
 from lxml import etree
 
-import cactus_runner.schema.csipaus12 as csipaus12
+import cactus_runner.schema.csipaus13 as csipaus13
 from cactus_runner.app.proxy import ProxyResult
 
 logger = logging.getLogger(__name__)
 
-CSIP_AUS_12_DIR = Path(csipaus12.__file__).parent
+CSIP_AUS_13_DIR = Path(csipaus13.__file__).parent
 
 
 class LocalXsdResolver(etree.Resolver):
     """Finds specific XSD files in our local schema directory"""
 
-    def resolve(self, url, id, context):
+    def resolve(self, url, _, context):
         if url == "sep.xsd":
-            return self.resolve_filename(str(CSIP_AUS_12_DIR / "sep.xsd"), context)
+            return self.resolve_filename(str(CSIP_AUS_13_DIR / "sep.xsd"), context)
         elif url == "csipaus-core.xsd":
-            return self.resolve_filename(str(CSIP_AUS_12_DIR / "csipaus-core.xsd"), context)
+            return self.resolve_filename(str(CSIP_AUS_13_DIR / "csipaus-core.xsd"), context)
         elif url == "csipaus-ext.xsd":
-            return self.resolve_filename(str(CSIP_AUS_12_DIR / "csipaus-ext.xsd"), context)
+            return self.resolve_filename(str(CSIP_AUS_13_DIR / "csipaus-ext.xsd"), context)
         return None
 
 
@@ -34,14 +34,14 @@ def csip_aus_schema() -> etree.XMLSchema:
     parser.resolvers.add(LocalXsdResolver())
 
     # Load schema
-    with open(CSIP_AUS_12_DIR / "csipaus-core.xsd", "r") as fp:
+    with open(CSIP_AUS_13_DIR / "csipaus-core.xsd", "r") as fp:
         xsd_content = fp.read()
     schema_root = etree.XML(xsd_content, parser)
     return etree.XMLSchema(schema_root)
 
 
 def validate_xml(xml: str) -> list[str]:
-    """Validates an xml document / snippet as a valid CSIP Aus 1.2 XML snippet. Returns a list of any human
+    """Validates an xml document / snippet as a valid CSIP Aus 1.3-beta/storage XML snippet. Returns a list of any human
     readable schema validation errors. Empty list means that xml is schema valid"""
 
     try:
